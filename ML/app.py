cfe289b48bdaa1fbcdc8e428ac87f0a9922f18d5
Arief Darmawan Tantriady <ariefdarmawantantriady@gmail.com>
2021-06-03 00:46:35 +0700

    flask for GCP server + machine learning model

---
+++ ML/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify
+import cv2
+from model import load_model
+import urllib
+import numpy as np
+from keras.preprocessing import image
+from PIL import Image
+
+app = Flask(__name__)
+
+model = load_model()
+labels = ['lentigo NOS','lichenoid keratosis', 'melanoma', 'nevus', 'seborrheic keratosis']
+
+@app.route('/', methods=['GET', 'POST'])
+def handle_request():
+    return "Server is running and connnected."
+
+@app.route('/predict', methods=['POST'])
+def predictor():
+    def image_from_url(url):
+        request = urllib.request.urlopen(url)
+        image = np.asarray(bytearray(request.read()), dtype="uint8")
+        image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+        return image
+
+    content = request.json
+    img = Image.fromarray(image_from_url(content["image_path"])).resize((128,128))
+    x=np.expand_dims(img, axis=0)
+    images = np.vstack([x])
+    classes = model.predict(images)
+    return jsonify(status='SUCCESS',predict=labels[np.argmax(classes[0])])
+    
+app.run(host="0.0.0.0", port=5000, debug=True)
---
+++ ML/model.py
@@ -0,0 +1,3 @@
+import tensorflow as tf
+def load_model():
+   return tf.keras.models.load_model('me_skin.h5')
