852263339cf3834b5f3cb2da087aa207fc252c3d
elsafir <49156274+elsafir@users.noreply.github.com>
2021-06-03 11:14:07 +0700

    camera newframe

--- AndroidCapstoneProject/app/src/main/java/com/dicoding/android_capstoneproject/ui/Detect/DetectFragment.kt
+++ AndroidCapstoneProject/app/src/main/java/com/dicoding/android_capstoneproject/ui/Detect/DetectFragment.kt
@@ -1,15 +1,12 @@
-package com.dicoding.android_capstoneproject.ui.home
+package com.dicoding.android_capstoneproject.ui.Detect
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.dicoding.android_capstoneproject.databinding.FragmentDetectBinding
-import com.dicoding.android_capstoneproject.ui.home.DetectViewModel
 
 
 class DetectFragment : Fragment() {
@@ -29,11 +26,6 @@ class DetectFragment : Fragment() {
 
         _binding = FragmentDetectBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
-        val textView: TextView = binding.textDashboard
-        detectViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
         return root
     }
 
