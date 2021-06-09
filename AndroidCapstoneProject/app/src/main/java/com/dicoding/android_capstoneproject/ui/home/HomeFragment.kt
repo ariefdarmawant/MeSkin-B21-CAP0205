a06723850b0c34e4c9957b9357f3134a554f969f
elsafir <49156274+elsafir@users.noreply.github.com>
2021-06-09 08:55:51 +0700

    udah bisa kameranya

--- AndroidCapstoneProject/app/src/main/java/com/dicoding/android_capstoneproject/ui/home/HomeFragment.kt
+++ AndroidCapstoneProject/app/src/main/java/com/dicoding/android_capstoneproject/ui/home/HomeFragment.kt
@@ -4,22 +4,22 @@ import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
 import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import coil.load
 import coil.transform.CircleCropTransformation
 import com.dicoding.android_capstoneproject.R
 import kotlinx.android.synthetic.main.fragment_home.*
+import java.util.jar.Manifest
 
 
-class HomeFragment : Fragment() {
-    companion object {
-        const val CAMERA_REQUEST_CODE = 1
-        const val GALLERY_REQUEST_CODE =2
-    }
+class HomeFragment() : Fragment() {
 
 
     override fun onCreateView(
@@ -42,46 +42,24 @@ class HomeFragment : Fragment() {
     private fun gallery() {
         val intent = Intent(Intent.ACTION_PICK)
         intent.type = "image/*"
-        startActivityForResult(intent, GALLERY_REQUEST_CODE)
+        startActivityForResult(intent, 1)
     }
 
     private fun camera() {
-        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+       val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+       startActivityForResult(intent, 2)
     }
 
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if (resultCode == Activity.RESULT_OK) {
-
-            when (requestCode) {
-
-                CAMERA_REQUEST_CODE -> {
-
-                    val bitmap = data?.extras?.get("data") as Bitmap
-
-                    //we are using coroutine image loader (coil)
-                    imageView.load(bitmap) {
-                        crossfade(true)
-                        crossfade(1000)
-                        transformations(CircleCropTransformation())
-                    }
-                }
-
-                GALLERY_REQUEST_CODE -> {
-
-                    imageView.load(data?.data) {
-                        crossfade(true)
-                        crossfade(1000)
-                        transformations(CircleCropTransformation())
-                    }
-
-                }
-            }
+        if (requestCode == 2) {
+            var bmp = data?.extras?.get("data") as Bitmap
+            imageView.setImageBitmap(bmp)
+        }else if(requestCode ==1){
+            imageView.setImageURI(data?.data)
 
         }
-
     }
-
 }
